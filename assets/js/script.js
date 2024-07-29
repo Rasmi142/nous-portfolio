4fa638078db82c930d435216a43aa633ea8b9551
Rasmiya <143290837+Rasmi142@users.noreply.github.com>
2024-07-29 10:08:49 +0000

    hero section added

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    init_loader();
+});
+
+/* --------------------------------------------
+   Page loader
+   --------------------------------------------- */
+   function init_loader(){   
+    $(".page-loader").delay(9500).slideUp("linear");
+}
